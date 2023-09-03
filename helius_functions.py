2d2fa61efd05fd7502d6107fb5564106a52e5a7d
Arnav Jain <64888928+BlurryFace04@users.noreply.github.com>
2023-09-04 01:50:13 +0530

    Update helius_functions.py

--- helius_functions.py
+++ helius_functions.py
@@ -1,15 +1,13 @@
 import requests
-import json
 import streamlit as st
 
 api_key = st.secrets.helius_api_key
+URL = f"https://rpc.helius.xyz/?api-key={api_key}"
 
 
 def fetch_nft_data(mint_addresses: list) -> list:
     if not isinstance(mint_addresses, list):
         mint_addresses = [mint_addresses]
-
-    url = f"https://rpc.helius.xyz/?api-key={api_key}"
 
     batch = [
         {
@@ -23,12 +21,58 @@ def fetch_nft_data(mint_addresses: list) -> list:
         for i, mint_address in enumerate(mint_addresses)
     ]
 
-    response = requests.post(url, headers={"Content-Type": "application/json"}, json=batch)
+    response = requests.post(URL, headers={"Content-Type": "application/json"}, json=batch)
     response.raise_for_status()
 
     return response.json()
 
 
-def save_to_json(data, filename="nft_data2.json"):
-    with open(filename, 'w') as json_file:
-        json.dump(data, json_file, indent=4)
+def extract_nft_data(nft: dict) -> dict:
+    data = {"mint_address": nft.get("id")}
+
+    content = nft.get("content", {})
+    metadata = content.get("metadata", {})
+
+    data["name"] = metadata.get("name")
+    data["symbol"] = metadata.get("symbol")
+    data["description"] = metadata.get("description")
+
+    links = content.get("links", {})
+    data["image"] = links.get("image")
+
+    return data
+
+
+def get_nfts_by_owner(address: str) -> list:
+    page_number = 1
+    limit = 1000
+    all_nfts = []
+
+    while True:
+        payload = {
+            "jsonrpc": "2.0",
+            "id": "my-id",
+            "method": "getAssetsByOwner",
+            "params": {
+                "ownerAddress": address,
+                "page": page_number,
+                "limit": limit,
+            },
+        }
+
+        response = requests.post(URL, headers={"Content-Type": "application/json"}, json=payload)
+        response_data = response.json()
+
+        if "result" in response_data and "items" in response_data["result"]:
+            nfts = response_data["result"]["items"]
+            processed_nfts = [extract_nft_data(nft) for nft in nfts]
+            all_nfts.extend(processed_nfts)
+
+            if len(nfts) < limit:
+                break
+
+            page_number += 1
+        else:
+            break
+
+    return all_nfts
